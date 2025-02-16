da6ed3a193f22295cb6a2c2cf6e9681a3543108e
den4ik2975 <den41k2975@yandex.ru>
2025-02-16 23:11:53 +0300

    feat: minor update

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Updated CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 @app.get("/api/hello")
