066fdde22825d1562302a6730484d44ddfcb3b9d
den4ik2975 <den41k2975@yandex.ru>
2025-04-21 14:22:27 +0300

    [api] feat: added analysis type choice

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,29 +1,35 @@
+from loguru import logger
+
 from app import TaskStatus, Highlight
+from app.core import ChessAnalysisInterface
 from app.db import get_sql_sessionmaker, SQLAlchemyUnitOfWork
 
 
 async def run_analysis(game_id: int, task_id: int):
+    logger.info(f"Running analysis for game with id: {game_id}")
+
     async with SQLAlchemyUnitOfWork(get_sql_sessionmaker()) as uow:
         try:
             task = await uow.task.get(task_id)
             task.status = TaskStatus.PROCESSING
             await uow.commit()
 
-            # game = await uow.game.get(game_id)
-            # result = await analyze_game(game.pgn_data)
+            game = await uow.game.get(game_id)
 
-            results = [["10W", "15B", "WOW horse dies"], [18, 20, "РокировОчка"]]
+            analysis = ChessAnalysisInterface()
+            results = await analysis.analyze_game(game.pgn_data)
 
             for result in results:
                 highlight = Highlight(
                     start_move=result[0],
                     end_move=result[1],
-                    description=result[2],
+                    description=result[2] if len(result) > 2 else "Not provided",
                     game_id=game_id
                 )
                 await uow.highlight.create(highlight)
 
             task.status = TaskStatus.COMPLETED
+            logger.info(f"Analysis completed for game with id: {game_id}")
             await uow.commit()
 
         except Exception as e:
